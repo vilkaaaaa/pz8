0a91ef0860df2d58870b42fb34d1de6426b3b600
vilkaaaaa <114383607+vilkaaaaa@users.noreply.github.com>
2025-01-25 13:00:45 +0500

    Update Calculator.cs

--- pz8/Calculator.cs
+++ pz8/Calculator.cs
@@ -11,12 +11,17 @@ namespace pz8
         public double Add(double a, double b)=>a+b;
         public double Sub(double a, double b) => a - b;
         public double Multiply(double a, double b) => a * b;
-        public double Divide(double a, double b)
-        {
-            if (b == 0) throw new DivideByZeroException("Деление на 0 - ошибка");
-            Console.WriteLine("Деление на 0 - ошибка");
-            return a/b;
-        }
+         public double Divide(double a, double b)
+  {
+      if (b == 0)
+      {
+          Console.WriteLine("Деление на 0 - ошибка");
+            throw new DivideByZeroException("Деление на 0 - ошибка");
+        
+      }
+      else{ return a / b; }
+      
+  }
         public double Sqrt(double a)=>Math.Sqrt(a);
         public double Pow(double a, double b) => Math.Pow(a,b);
     }
